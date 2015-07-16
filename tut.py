8cbece718b64860d606ff93a36801eeb3178f9c2
Sadanand Singh <sadanand4singh@gmail.com>
2015-07-15 22:55:54 -0700

    Added tut.py example main script

--- tut.py
+++ tut.py
@@ -14,8 +14,9 @@ last edited: January 2015
 """
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
 from ludoWindow import Ui_LudoWindow
+from PyQt5 import QtCore, QtGui
 
 class LudoWindow(QMainWindow, Ui_LudoWindow):
     def __init__(self, parent=None):
@@ -26,6 +27,12 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = LudoWindow()
+    f = QtGui.QFont( "Arial", 10, QtGui.QFont.Bold)
+    stat = QLabel("Sadanand You Got 6")
+    stat.setFont(f)
+    stat.setAlignment(QtCore.Qt.AlignCenter)
+    ex.statusbar.addWidget(stat, 1)
+
     ex.show()
-    print("Hi")
+
     sys.exit(app.exec_())
