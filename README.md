084709b9c7f34ffe55d1ea854035632c1d066a9a
Sadanand Singh <sadanand4singh@gmail.com>
2015-07-15 22:24:27 -0700

    Updated files

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is my first development in QT.
 I will be using pyqt5 to develop a game of Ludo.
 
 Ludo is a board game quite popular in the Indian
-subcontinent. You read more about this game at
+subcontinent. You can read more about this game at
 [Wiki](https://en.wikipedia.org/wiki/Ludo_%28board_game%29).
 
 ![Ludo Board](http://www.chiquiplanet.com/board_games/rec/ludo.gif)
--- tut.py
+++ tut.py
@@ -14,25 +14,18 @@ last edited: January 2015
 """
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication, QPushButton
-from PyQt5.QtGui import QIcon
-from mainWindow import Ui_LudoWindow
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from ludoWindow import Ui_LudoWindow
 
-class MainWindow(QMainWindow, Ui_LudoWindow):
+class LudoWindow(QMainWindow, Ui_LudoWindow):
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
+        super(LudoWindow, self).__init__(parent)
         self.setupUi(self)
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    ex = MainWindow()
-
-    positions = [(i,j) for i in range(13) for j in range(13)]
-
-    for position in positions:
-        button = QPushButton("s")
-        ex.gridLayout.addWidget(button,*position)
-
+    ex = LudoWindow()
     ex.show()
+    print("Hi")
     sys.exit(app.exec_())
