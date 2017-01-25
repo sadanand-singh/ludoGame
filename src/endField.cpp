262ae4171c53199aae0f69f40773373db276e234
Sadanand Singh <sadanand4singh@gmail.com>
2017-01-24 19:39:19 -0800

    add endField.h/cpp files for baord end definition

---
+++ src/endField.cpp
@@ -0,0 +1,7 @@
+#include "endField.h"
+#include "figure.h"
+
+EndField::EndField(qreal x, qreal y, qreal w, qreal h, QGraphicsItem* parent) :
+    Field(x, y, w, h, parent)
+{}
+
---
+++ src/endField.h
@@ -0,0 +1,16 @@
+#ifndef ENDFIELD_HEADER
+#define ENDFIELD_HEADER
+
+#include "field.h"
+
+class EndField : public Field
+{
+    Q_OBJECT
+
+    public:
+    EndField(qreal x, qreal y, qreal w, qreal h, QGraphicsItem* parent = nullptr);
+
+    // virtual void setColor(QColor color) Q_DECL_OVERRIDE;
+};
+
+#endif
